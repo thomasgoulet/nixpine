f3f02509d84168ad2b1015ebec203e8871e95ba3
thomasgoulet <thomas@jfgoulet.com>
2022-11-29 23:29:25 -0500

    Adding default packages

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
   
   in {
     
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
     homeConfigurations = {
       thomas = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
